29bb9b9452d755c4cd436cc0a5a5f2cf25d8cfa5
natanael986 <99516017+natanael986@users.noreply.github.com>
2022-03-16 17:01:10 -0300

    Update index.php

--- Lista_1/Exer_1/index.php
+++ Lista_1/Exer_1/index.php
@@ -9,8 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Exercício 1</title>
-</head>
+    <title>LISTA 1</title>
 
 <body class="container">
     <h1>Exercício 1</h1>
