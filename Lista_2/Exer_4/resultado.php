0faa89b7c5359ac66d0132c78cdc37015dc41773
natanael986 <99516017+natanael986@users.noreply.github.com>
2022-03-20 23:43:34 -0300

    lista2

--- Lista_2/Exer_4/resultado.php
+++ Lista_2/Exer_4/resultado.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,11 +9,26 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
-  </head>
-  <body class="container" >
-    <h1>Hello, world!</h1>
+    <title>Lista 2</title>
+</head>
 
+<body class="container">
+    <h1>Resultado ex 4</h1>
+    <div class="border-top border-start">
+        <div class="mt-2 ms-2"  >
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+            $vetor[$i] = $_POST["valor$i"];
+        }
+
+        $valores = 0;
+        foreach ($vetor as $chave => $valor) {
+            $valores = $valores + $valor;
+        }
+
+        echo "A média dos valores digitados é: " . ($valores / 10);
+        ?>
+    </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -23,5 +39,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
