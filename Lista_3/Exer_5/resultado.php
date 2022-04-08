b44c01836b268216a064296112a2d4b84e5f5b02
natanael986 <99516017+natanael986@users.noreply.github.com>
2022-04-08 16:13:07 -0300

    lista 3

--- Lista_3/Exer_5/resultado.php
+++ Lista_3/Exer_5/resultado.php
@@ -14,61 +14,65 @@
 
 <body class="container">
     <h1>Resultado ex 5</h1>
-    <div class="col-5 mt-4 ms-5">
-        <table class="table text-center">
-            <thead class="table-primary ">
-                <tr>
-                    <th scope="col">IMC</th>
-                    <th scope="col">Classificação</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th class="table-primary" scope="row">abaixo de 18,5</th>
-                    <td class="table-secondary">abaixo do peso</td>
-                </tr>
-                <tr>
-                    <th class="table-primary" scope="row">entre 18,6 e 24,9</th>
-                    <td>Peso ideal(parabéns)</td>
-                </tr>
-                <tr>
-                    <th class="table-primary" scope="row">entre 25,0 e 29,9</th>
-                    <td class="table-secondary">Levemente acima do peso</td>
-                </tr>
-                <tr>
-                    <th class="table-primary" scope="row">entre 30,0 e 34,9</th>
-                    <td>Obesidade grau I</td>
-                </tr>
-                <tr>
-                    <th class="table-primary" scope="row">entre 35,0 e 39,9</th>
-                    <td class="table-secondary">Obesidade grau II(severa)</td>
-                </tr>
-                <tr>
-                    <th class="table-primary" scope="row">acima 40</th>
-                    <td>Obesidade grau III(mórbida)</td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="row">
+        <div class="col-5 mt-4 ms-5">
+            <table class="table text-center">
+                <thead class="table-primary ">
+                    <tr>
+                        <th scope="col">IMC</th>
+                        <th scope="col">Classificação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th class="table-primary" scope="row">abaixo de 18,5</th>
+                        <td class="table-secondary">abaixo do peso</td>
+                    </tr>
+                    <tr>
+                        <th class="table-primary" scope="row">entre 18,6 e 24,9</th>
+                        <td>Peso ideal(parabéns)</td>
+                    </tr>
+                    <tr>
+                        <th class="table-primary" scope="row">entre 25,0 e 29,9</th>
+                        <td class="table-secondary">Levemente acima do peso</td>
+                    </tr>
+                    <tr>
+                        <th class="table-primary" scope="row">entre 30,0 e 34,9</th>
+                        <td>Obesidade grau I</td>
+                    </tr>
+                    <tr>
+                        <th class="table-primary" scope="row">entre 35,0 e 39,9</th>
+                        <td class="table-secondary">Obesidade grau II(severa)</td>
+                    </tr>
+                    <tr>
+                        <th class="table-primary" scope="row">acima 40</th>
+                        <td>Obesidade grau III(mórbida)</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-5 border-start border-top mt-4 ms-5">
+            <?php
+            $altura = $_POST['alt'];
+            $peso = $_POST['peso'];
+            $imc = 0;
+            $imc = $peso / ($altura * $altura);
+            $form_num = number_format($imc, 1, '.', '');
+            if ($imc <= 18.5) {
+                echo "Seu IMC $form_num, você está abaixo do peso!<br/>";
+            } else if ($imc >= 18.6 and $imc <= 24.9) {
+                echo "Seu IMC $form_num, você está no peso ideal (parabéns)!<br/>";
+            } else if ($imc >= 25 and $imc <= 29.9) {
+                echo "Seu IMC $form_num, você está levemente acima do peso!<br/>";
+            } else if ($imc >= 30 and $imc <= 34.9) {
+                echo "Seu IMC $form_num, obesidade grau I!<br/>";
+            } else if ($imc >= 35 and $imc <= 39.9) {
+                echo "Seu IMC $form_num, obesidade grau II (severa)!<br/>";
+            } else
+                echo "Seu IMC $form_num, Obesidade grau III (mórbida)!<br/>";
+            ?>
+        </div>
     </div>
-    <?php
-    $altura = $_POST['alt'];
-    $peso = $_POST['peso'];
-    $imc = 0;
-    $imc = $peso / ($altura * $altura);
-    $form_num = number_format($imc, 1, '.', '');
-    if ($imc <= 18.5) {
-        echo "Seu IMC $form_num, você está abaixo do peso!<br/>";
-    } else if ($imc >= 18.6 and $imc <= 24.9) {
-        echo "Seu IMC $form_num, você está no peso ideal (parabéns)!<br/>";
-    } else if ($imc >= 25 and $imc <= 29.9) {
-        echo "Seu IMC $form_num, você está levemente acima do peso!<br/>";
-    } else if ($imc >= 30 and $imc <= 34.9) {
-        echo "Seu IMC $form_num, obesidade grau I!<br/>";
-    } else if ($imc >= 35 and $imc <= 39.9) {
-        echo "Seu IMC $form_num, obesidade grau II (severa)!<br/>";
-    } else
-        echo "Seu IMC $form_num, Obesidade grau III (mórbida)!<br/>";
-    ?>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
